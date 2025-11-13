f3046f8a76fdcf17777d94da99dafde13c05dca1
Inacio Carvalho <inacio.carvalho.oliveira02@gmail.com>
2025-11-12 22:16:35 -0300

    fix: test actions git

---
+++ Interfaces/UserContactRepository.cs
@@ -0,0 +1,101 @@
+using System.Text.Json;
+using ContactKeeper.Data;
+using ContactKeeper.Models;
+using ContactKeeper.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace ContactKeeper.Ineterfaces
+{
+    public class UserContactRepository : IUserContactRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMemoryCache _cache;
+
+        public UserContactRepository(DataContext context, IMemoryCache cache)
+        {
+            _context = context;
+            _cache = cache;
+        }
+        public async Task<UserContact> AddUserContact(UserContact userContact)
+        {
+            string path = "d:/GitHub/Dotnet/APIS/ContactKeeper/util/ddd/dddsBrasileiros.json";
+            var jsonString = await File.ReadAllTextAsync(path);
+            var dddsData = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, List<string>>>>(jsonString);
+
+            if (dddsData != null && dddsData["dddsPorEstado"].Values.Any(list => list.Contains(userContact.DDD.ToString())))
+            {
+
+                var user = await _context.Users
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(u => u.Id == userContact.UserId);
+
+                if (user == null || user.Id != userContact.UserId)
+                {
+                    throw new Exception("cant add user contact, user not found");
+                }
+                _context.UserContacts.Add(userContact);
+                await _context.SaveChangesAsync();
+
+                return userContact;
+
+
+            }
+            else
+            {
+                throw new ArgumentException("DDD inválido.");
+            }
+        }
+
+        public async Task<IEnumerable<UserContact>> GetUserContact()
+        {
+            var userContacts = await _context.UserContacts
+                .AsNoTracking()
+                .ToListAsync();
+
+            return userContacts;
+        }
+
+        public async Task<UserContact> DeleteUserContact(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserContact> GetUserContactById(int userId)
+        {
+            var userContact = await _context.UserContacts
+                .AsNoTracking()
+                .FirstOrDefaultAsync(uc => uc.UserId == userId);
+            return userContact;
+        }
+
+        public async Task<UserContact> UpdateUserContact(UserContact userContact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IList<UserContact>> GetInfoUserByDdd(int ddd)
+        {
+            const string cacheKey = "UsersByDdd";
+            if (!_cache.TryGetValue(cacheKey, out IList<UserContact> userContacts))
+            {
+                userContacts = await _context.UserContacts
+                .AsNoTracking()
+                .Where(x => x.DDD == ddd)
+                .ToListAsync();
+
+                _cache.Set(cacheKey, userContacts, new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10)
+                });
+                Console.WriteLine("dados do banco");
+            }
+            else
+            {
+                userContacts = _cache.Get(cacheKey) as IList<UserContact>;
+                Console.WriteLine("dados do cache");
+            }
+            return userContacts;
+        }
+    }
+}
---
+++ Migrations/20251019010923_initialCreate.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactKeeper.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserContact",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    DDI = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    DDD = table.Column<int>(type: "int", nullable: false),
+                    LocalNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserContact", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserContact_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserContact_UserId",
+                table: "UserContact",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserContact");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}

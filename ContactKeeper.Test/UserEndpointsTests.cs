f662bbf0428b93bc27ca02b0b4015ac6fa4a6b4a
Inacio Carvalho <inacio.carvalho.oliveira02@gmail.com>
2026-06-01 01:26:43 -0300

    ajuste de  senha

--- ContactKeeper.Test/UserEndpointsTests.cs
+++ ContactKeeper.Test/UserEndpointsTests.cs
@@ -88,7 +88,7 @@ namespace ContactKeeper.Test
             var payload = new
             {
                 username = "inacio",
-                password = "12345678",
+                password = "12345678910",
                 role = "admin"
             };
 

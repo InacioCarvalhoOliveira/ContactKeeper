b9948466d06bda650e0805ec15cc2ff62f9dcd63
Inacio Carvalho <inacio.carvalho.oliveira02@gmail.com>
2026-07-02 12:48:50 -0300

    build: change visibility on aks and local actions

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
     {
-        Title = "ContactKeeper API V2",
+        Title = "ContactKeeper API V3",
         Version = "v1",
         Description = "API para Cadastro e consulta de Contatos telefônicos",
         Contact = new OpenApiContact
